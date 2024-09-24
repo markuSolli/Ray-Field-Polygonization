163417b07f7a133e88a03c1f9a449480ed3f01bd
Markus Solli Pedersen <72740992+markuSolli@users.noreply.github.com>
2024-09-24 15:56:48 +0200

    Fixed draw_geometries by downgrading numpy to 1.24 (#7)
    Co-authored-by: Markus Solli Pedersen <markussp@stud.ntnu.no>

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Ray Field Polygonization
+Python Version: 3.11.9
--- main.py
+++ main.py
@@ -83,7 +83,7 @@ mesh: Trimesh = trimesh.load_mesh('suzanne.obj')
 
 # Scale down mesh to fit unit circle
 scale: ndarray[float64] = mesh.extents
-transform: ndarray[float64] = trimesh.transformations.scale_matrix(2 / np.max(scale))
+transform: ndarray[float64] = trimesh.transformations.scale_matrix(2.0 / np.max(scale))
 mesh.apply_transform(transform)
 
 # Generate points along the unit sphere
@@ -98,11 +98,17 @@ locations, index_ray, index_tri = mesh.ray.intersects_location(ray_origins=rays[
 # Get face normals for intersection points
 normals: ndarray = mesh.face_normals[index_tri]
 
+# Create Open3D point cloud with normals
 point_cloud = o3d.geometry.PointCloud()
 point_cloud.points = o3d.utility.Vector3dVector(locations)
 point_cloud.normals = o3d.utility.Vector3dVector(normals)
 
+# Run Poisson Surface Reconstruction
 with o3d.utility.VerbosityContextManager(o3d.utility.VerbosityLevel.Debug) as cm:
-    mesh, densities = o3d.geometry.TriangleMesh.create_from_point_cloud_poisson(point_cloud, depth=9)
+    generated_mesh, densities = o3d.geometry.TriangleMesh.create_from_point_cloud_poisson(point_cloud)
 
-o3d.visualization.draw_geometries([mesh])
+generated_mesh.compute_vertex_normals()
+generated_mesh.paint_uniform_color(np.array([[0.5],[0.5],[0.5]]))
+
+# Visualize
+o3d.visualization.draw_geometries([generated_mesh])
