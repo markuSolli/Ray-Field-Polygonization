519ee773cfff75f8f70b1d4f8723a1a823b2eee5
Markus Solli Pedersen <72740992+markuSolli@users.noreply.github.com>
2024-10-08 17:06:05 +0200

    Visualize steps (#12)
    * Script for visualizing process
    
    * Fixed object fitting to unit sphere
    
    * Set multiple hits to false
    
    ---------
    
    Co-authored-by: Markus Solli Pedersen <markussp@stud.ntnu.no>

--- n_points_plot.py
+++ n_points_plot.py
@@ -12,14 +12,14 @@ from numpy import ndarray
 
 POISSON_DEPTH = 8
 DATA_DIR = 'n_points'
-OBJECT_NAME = 'suzanne'
+OBJECT_NAME = 'spot'
 
 def plot_results(x: list[int], y: list[float]) -> None:
     fig, ax = plt.subplots()
     ax.plot(x, y)
-    ax.set_ylim([0, 0.04])
+    ax.set_ylim([0, 0.045])
     ax.set_ylabel('chamfer distance')
-    ax.set_xlabel('n')
+    ax.set_xlabel('n points')
     ax.set_title(OBJECT_NAME)
     fig.savefig(f'data/{DATA_DIR}/{OBJECT_NAME}_plot.svg')
     plt.show()
