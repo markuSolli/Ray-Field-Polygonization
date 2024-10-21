ed5cd77140fa15903886db540d38c83b593a44af
Markus Solli Pedersen <72740992+markuSolli@users.noreply.github.com>
2024-10-21 15:29:25 +0200

    Swap rocker-arm with suzanne (#14)
    Co-authored-by: Markus Solli Pedersen <markussp@stud.ntnu.no>

--- n_points_plot.py
+++ n_points_plot.py
@@ -17,7 +17,7 @@ OBJECT_NAME = 'spot'
 def plot_results(x: list[int], y: list[float]) -> None:
     fig, ax = plt.subplots()
     ax.plot(x, y)
-    ax.set_ylim([0, 0.045])
+    ax.set_ylim([0, 0.05])
     ax.set_ylabel('chamfer distance')
     ax.set_xlabel('n points')
     ax.set_title(OBJECT_NAME)
