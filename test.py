b96d8b45087b6fe4dd50c0608bd353d023df80b4
Markus Solli Pedersen <markusped@hotmail.com>
2025-02-17 12:29:52 +0100

    Candidate sphere prototype

--- test.py
+++ test.py
@@ -1,9 +1,11 @@
 import torch
+import trimesh
+import numpy as np
 from ray_field import utils
 
 model, device = utils.init_model('bunny')
 with torch.no_grad():
-    origins, dirs = utils.generate_sphere_rays_tensor(60, device)
+    origins, dirs = utils.generate_sphere_rays_tensor(300, device)
 
     result = model.forward(dict(origins=origins, dirs=dirs), intersections_only = False, return_all_atoms = True)
 
@@ -30,8 +32,8 @@ with torch.no_grad():
     print(f'intersection_normals:\t\t{intersection_normals.shape}')
     print(f'is_intersecting:\t\t{is_intersecting.shape}')
     print(f'sphere_centers:\t\t\t{sphere_centers.shape}')
-
     print(f'sphere_radii:\t\t\t{sphere_radii.shape}')
+
     print(f'atom_indices:\t\t\t{atom_indices.shape}')
     print(f'all_intersections:\t\t{all_intersections.shape}')
     print(f'all_intersection_normals:\t{all_intersection_normals.shape}')
@@ -40,3 +42,51 @@ with torch.no_grad():
     print(f'all_is_intersecting:\t\t{all_is_intersecting.shape}')
     print(f'all_sphere_centers:\t\t{all_sphere_centers.shape}')
     print(f'all_sphere_radii:\t\t{all_sphere_radii.shape}')
+
+is_intersecting = is_intersecting.flatten()
+intersections = intersections.flatten(end_dim=2)[is_intersecting].detach().numpy()
+atom_indices = atom_indices.flatten()[is_intersecting].detach().numpy()
+
+candidate_centers = np.zeros((16, 3), dtype=float)
+candidate_n = np.zeros(16, dtype=int)
+
+for i in range(intersections.shape[0]):
+    index = atom_indices[i]
+    candidate_centers[index] = candidate_centers[index] + intersections[i]
+    candidate_n[index] = candidate_n[index] + 1
+
+for i in range(16):
+    if (candidate_n[i] > 0):
+        candidate_centers[i] = candidate_centers[i] / candidate_n[i] 
+
+max_radii = np.zeros(16, dtype=float)
+
+for i in range(intersections.shape[0]):
+    index = atom_indices[i]
+    distance = np.linalg.norm(intersections[i] - candidate_centers[index])
+
+    if distance > max_radii[index]:
+        max_radii[index] = distance
+
+candidate_spheres = []
+for i in range(16):
+    if (candidate_n[i] > 0 and max_radii[i] > 0):
+        r = (i % 4) * 64
+        g = (i % 6) * 43
+        b = (i % 8) * 32
+
+        sphere = trimesh.primitives.Sphere(max_radii[i], candidate_centers[i])
+        sphere.visual.face_colors[:] = np.array([r, g, b, 64])
+        candidate_spheres.append(sphere)
+
+def index_to_color(index: int):
+    r = (index % 4) * 64
+    g = (index % 6) * 43
+    b = (index % 8) * 32
+
+    return [r, g, b]
+
+colors = np.array([index_to_color(idx) for idx in atom_indices])
+surface_cloud = trimesh.points.PointCloud(intersections, colors=colors)
+scene = trimesh.Scene([surface_cloud])
+scene.show()
